9aa06d2fc332de4af76bece6079deb61565b24ee
mnsgl <ozcan85114@ogr.duzce.edu.tr>
2021-05-27 20:16:26 +0300

    fixed export problem

--- pages/api/set/set-name.js
+++ pages/api/set/set-name.js
@@ -1,15 +1,15 @@
 import { MongoClient } from "mongodb";
 
 export default async function handler(req, res) {
-  let ret = null
-  if(req.method === "POST") {
-    ret = await postMethod(req, res)
+  let ret = null;
+  if (req.method === "POST") {
+    ret = await postMethod(req, res);
   } else {
-    ret = res.status(400).json({message: "Wrong api call"})
+    ret = res.status(400).json({ message: "Wrong api call" });
   }
 }
 
-export default async function postMethod(req, res) {
+async function postMethod(req, res) {
   let client = await MongoClient.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -18,10 +18,8 @@ export default async function postMethod(req, res) {
   let user = await db.collection("users").find({ name: userName }).toArray();
   let setsId = user[0].setsId;
   console.log(setsId);
-  return res.status(400).json({message: "Wrong api call"})
+  return res.status(400).json({ message: "Wrong api call" });
 }
-
-
 
 let url =
   "mongodb+srv://pyloo:Salamander.123@cluster0.t25mg.mongodb.net/WordApp?retryWrites=true&w=majority";
